dd323fbb229b9b82cc34e03746408aa0b87764f4
Isaac Overcast <isaac.overcast@gmail.com>
2018-04-24 18:19:29 -0400

    Removed some detritus from the post-link process, and moved makeMeta.R to the util directory.

---
+++ util/makeMeta.R
@@ -0,0 +1,34 @@
+## function to make the mainland meta community with a phylo, traits, and abundances
+## required packages:
+##    ape,
+##    TreeSim,
+##    meteR
+## arguments:
+#' @param Jm the number of individuals in the meta community
+#' @param S the number of species in the meta community
+#' @param lambda the speciation rate
+#' @param deathFrac the proportional extinction rate
+#' @param sigma2 the rate of brownian motion
+
+makeMeta <- function(Jm, S, lambda, deathFrac, sigma2) {
+  ## the tree
+  tre <- TreeSim::sim.bd.taxa(S, numbsim = 1, lambda = lambda, mu = lambda * deathFrac,
+                              complete = FALSE)[[1]]
+
+  ## the traits
+  trt <- ape::rTraitCont(tre, sigma = sqrt(sigma2))
+  trt <- data.frame(name = names(trt), value = as.numeric(trt))
+
+  ## parameters for the log-series
+  nBar <- Jm / S
+  p <- 1 - 1/nBar
+  b <- -log(p)
+
+  ## the abundances
+  #abund <- pika::rfish(length(trt), 0.01)
+  abund <- sads::rls(length(trt), length(trt), 0.01)
+
+  ## return it all in a list
+  tre <- ape::write.tree(tre)
+  return(list(phylo = tre, traits = trt, abundance = abund))
+}
