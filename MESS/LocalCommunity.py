229ee9e2fd6668fb49521b7e3c1d89c324e54db5
Megan Ruffley <31750752+ruffleymr@users.noreply.github.com>
2018-07-03 12:29:24 -0700

    committing

--- MESS/LocalCommunity.py
+++ MESS/LocalCommunity.py
@@ -557,11 +557,7 @@ class LocalCommunity(object):
             ##############################################
             if self.region.paramsdict["speciation_model"] != "none" and\
                np.random.random_sample() < self.paramsdict["speciation_probability"]:
-<<<<<<< HEAD
-                self.speciate()
-=======
                self.speciate()
->>>>>>> f58f1b4c0d98147f3b3c476549d8654f589d6f38
 
             ## update current time
             self.current_time += 1
@@ -702,7 +698,7 @@ class LocalCommunity(object):
                 local_abund = self.local_community.count(name)
                 tdiv = self.current_time - coltime
                 ## Rescale abundances through time so they are "backwards" values
-                abundances_through_time = {self.current_time - x:y for x, y in list(self.local_info[name]["abundances_through_time"].items())} 
+                abundances_through_time = {self.current_time - x:y for x, y in list(self.local_info[name]["abundances_through_time"].items())}
                 sp = species(name = name,
                              colonization_time = tdiv,\
                              growth = self.region.paramsdict["population_growth"],\
