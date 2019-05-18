c96bfef81a8984b26c50dcff7be3ffa9c99e27a8
Isaac Overcast <isaac.overcast@gmail.com>
2019-05-18 11:42:03 -0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,7 @@ exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+autodoc_mock_imports = ["msprime"]
 
 # -- Options for HTML output -------------------------------------------------
 
