53b0dfac2123d057141fb4fd9ff9e0f1e415a4ed
Isaac Overcast <isaac.overcast@gmail.com>
2020-02-27 03:43:26 -0500

    Mock import rpy2 so the api docs will build

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-autodoc_mock_imports = ["msprime", "dendropy", "sklearn", "boruta", "skgarden", "joblib"]
+autodoc_mock_imports = ["msprime", "dendropy", "sklearn", "boruta", "skgarden", "joblib", "rpy2"]
 
 # -- Options for HTML output -------------------------------------------------
 
