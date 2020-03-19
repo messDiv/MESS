corrects bug in normalization of death_probabilities

MESS/LocalCommunity.py
@@ -598,9 +598,11 @@ class LocalCommunity(object):
             ## Get the victim conditioning on unequal death probability
             try:
                 vic_idx = list(np.random.multinomial(1, death_probs)).index(1)
-            except: 
+            except Exception as inst:
+                raise MESSError("Error in _deathsteap - {}".format(inst))
             ## vic_idx is nan because of too high values in the exponential
                 vic_idx = random.randint(0,self.paramsdict["J"]-1)
+                raise MESSError
             victim = self.local_community[vic_idx]
         if (not self.current_time % (self.paramsdict["J"]*2)) and self.fancy:
             self._record_deaths_probs(death_probs)
@@ -623,8 +625,12 @@ class LocalCommunity(object):
 
 
     def normalize(self, death_probs):
-        death_probs = death_probs + np.min(death_probs)
-        return death_probs/np.sum(death_probs)
+        death_probs = death_probs + abs(np.min(death_probs))
+        if np.sum(death_probs)==0:
+            return [1/self.paramsdict["J"] for _ in range(self.paramsdict["J"])]
+            # All equals
+        else:
+            return death_probs/np.sum(death_probs)
 
 
     def _filtering_death_step(self):

MESS/Metacommunity.py
@@ -232,8 +232,12 @@ class Metacommunity(object):
 
             elif param == "intrasp_competition":
                 if newvalue=="*":
-                    alpha = np.random.gamma(0.25,4)
-                    beta = np.random.gamma(0.25,4)
+                    mean = np.random.uniform(np.log10(0.01), np.log10(10), size=1)
+                    mean = np.power(10, mean)
+                    var = np.random.uniform(np.log10(0.01), np.log10(10), size=1)
+                    var = np.power(10, var)
+                    beta = var/mean
+                    alpha = mean/beta
                     newvalue = (alpha,beta)
                 # Aribitrary priors here ! Should be tuneable from parameter file !
                 tup = tuplecheck(newvalue, dtype=float)
@@ -242,8 +246,12 @@ class Metacommunity(object):
 
             elif param == "intersp_competition":
                 if newvalue=="*":
-                    alpha = np.random.gamma(0.25,4)
-                    beta = np.random.gamma(0.25,4)
+                    mean = np.random.uniform(np.log10(0.01), np.log10(10), size=1)
+                    mean = np.power(10, mean)
+                    var = np.random.uniform(np.log10(0.01), np.log10(10), size=1)
+                    var = np.power(10, var)
+                    beta = var/mean
+                    alpha = mean/beta
                     newvalue = (alpha,beta)
                 # Aribitrary priors here ! Should be tuneable from parameter file !
                 tup = tuplecheck(newvalue, dtype=float)
@@ -273,17 +281,7 @@ class Metacommunity(object):
 
 
     def _get_interaction_term(self,species1,species2):
-        
         return self.interaction_matrix[self.species_dict[species1]][self.species_dict[species2]]
-    #    try:
-    #         return self.interaction_matrix[int(float(species1))-1][int(float(species2))-1]
-    # #Species from 1 to 100 but matrix from 0 to 99
-    #     except ValueError:
-    #         if species1 in self.added_species:
-    #             species1 = self.added_species[species1]
-    #         if species2 in self.added_species:
-    #             species2 = self.added_species[species2]
-    #         return self.interaction_matrix[int(float(species1))-1][int(float(species2))-1]
 
     def _create_interaction(self,new, parent):
         ## Extend matrix